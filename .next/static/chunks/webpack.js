c97b079cd6aebdd0c10fdf448d7381988648aa73
guiilherme estrela <guilhermestrela@gmail.com>
2025-11-03 08:31:36 -0300

    fix: resolve hot reload issues
    - Fix webpack cache issues
    - Fix next.js trace
    - Fix prettier ignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1bc67d9083784a61"; }
+/******/ 		__webpack_require__.h = function() { return "2a0256070497ddb9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
