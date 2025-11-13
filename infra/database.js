828215e68bcbf603b4f09947ea98cfc424343c65
guilherme estrela <guilhermestrela@gmail.com>
2025-11-13 15:55:57 -0300

    add ssl on database

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,9 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "production"
+        ? { rejectUnauthorized: false }
+        : false,
   });
   console.log("Credenciais do Postgres:",{
     host: process.env.POSTGRES_HOST,
