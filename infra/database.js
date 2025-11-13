0cc8d21a0c650d917a27c9605699a17b9a71930d
guilherme estrela <guilhermestrela@gmail.com>
2025-11-13 16:23:01 -0300

    fix: correct casing of 'production' in SSL configuration

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "Production"
+    ssl: process.env.NODE_ENV === "production"
         ? { rejectUnauthorized: false }
         : false,
   });
